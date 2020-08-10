2f641e921c88974e013e26dbd31b0dc31519c811
Ace Young <luadengames@gmail.com>
2020-08-10 16:32:45 +0200

    Auto stash before merge of "Lua" and "origin/Lua"

--- Assets/Scripts/IGridManager.cs
+++ Assets/Scripts/IGridManager.cs
@@ -7,14 +7,7 @@ public interface IGridManager
     bool TryGetCell(Vector3Int threeAxis, out ICell foundCell);
     IEnumerable<ICell> TryGetCells(Vector3Int origin, IEnumerable <Vector3Int> threeAxisList);
     IEnumerable<ICell> TryGetCells(Vector3Int origin, uint radius = 1);
-    void GenerateGrid(uint TileMapId, int gridHeight, MapShape mapShape);
+    void GenerateGrid(uint TileMapId, uint gridHeight, MapShape mapShape);
     void ChangeTileInfo(ICell cell, TileInfo info);
     void ChangeTileOccupant(ICell cell, IOccupant occupant);
-}
-
-public interface IGridRenderer
-{
-    void PaintTile(uint TileMapId, IEnumerable<ICell> cells, TileInfo info = default, bool clearFirst = false);
-    void PaintTile(uint TileMapId, ICell cell, TileInfo info = default);
-    void PaintTile(uint TileMapId, Vector3Int gridVector, TileInfo info = default);
 }
---
+++ Assets/Scripts/IGridRenderer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGridRenderer
+{
+    void PaintTile(uint TileMapId, IEnumerable<ICell> cells, uint tileId = uint.MaxValue);
+    void PaintTile(uint TileMapId, ICell cell, uint tileId = uint.MaxValue);
+    void PaintTile(uint TileMapId, Vector3Int gridVector, uint tileId = uint.MaxValue);
+}
